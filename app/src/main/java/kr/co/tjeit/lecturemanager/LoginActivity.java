63b56ff9660248595d27e3007a21ed65400e7bca
henylee <asqaws1@gmail.com>
2017-08-31 10:39:09 +0900

    협업하고 주석달고 브런치기능 / 카카오톡 로그인모드 토스트 띄우기

--- app/src/main/java/kr/co/tjeit/lecturemanager/LoginActivity.java
+++ app/src/main/java/kr/co/tjeit/lecturemanager/LoginActivity.java
@@ -5,6 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.response.model.UserProfile;
+import com.kakao.util.exception.KakaoException;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,12 +21,16 @@ public class LoginActivity extends AppCompatActivity {
     private Button loginBtn;
 
     public static LoginActivity myActivity;
+    KakaoSessionCallback ksc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         myActivity = this;
+
+        ksc = new KakaoSessionCallback();
+        Session.getCurrentSession().addCallback(ksc);
 
         signUpBtn = (Button) findViewById(R.id.signUpBtn);
         loginBtn = (Button) findViewById(R.id.loginBtn);
@@ -38,6 +51,50 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+            return;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+    }
+
+    private class KakaoSessionCallback implements ISessionCallback {
+
+        @Override
+        public void onSessionOpened() {
+
+            UserManagement.requestMe(new MeResponseCallback() {
+
+                @Override
+                public void onSessionClosed(ErrorResult errorResult) {
+
+                }
+
+                @Override
+                public void onNotSignedUp() {
+
+                }
+
+                @Override
+                public void onSuccess(UserProfile result) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(LoginActivity.this, result.getNickname()+"님이 로그인했습니다.", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            });
+
+        }
+
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+
+        }
     }
 }
--- app/src/main/java/kr/co/tjeit/lecturemanager/SplashActivity.java
+++ app/src/main/java/kr/co/tjeit/lecturemanager/SplashActivity.java
@@ -1,9 +1,17 @@
 package kr.co.tjeit.lecturemanager;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -11,6 +19,20 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("kr.co.tjeit.lecturemanager", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
